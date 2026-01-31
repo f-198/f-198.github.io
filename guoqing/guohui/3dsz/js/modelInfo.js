18c09e8bc297171739c5fbb8acdd23c386ba2a4a
TermuxBot <termux@bot.local>
2026-01-31 14:36:00 +0800

    Add 15 files

---
+++ guoqing/guohui/3dsz/js/modelInfo.js
@@ -0,0 +1,49 @@
+
+
+        window.getFeature=function(){
+            return new Promise((resolve,reject)=>{
+                let info={"featureName":"国徽烘焙2021(3)","featureID":"24c1039e-a755-4694-a963-81947c9c67b1","description":"","thumbnail":null,"type":"0","createTime":"2025-04-03T17:35:51.9278364+08:00","mD5":"f639933467874934a469fc73aa42f875","status":0,"phase":"0","path":null,"currentVersion":0,"platform":"3ds Max","platformVersion":"2016","meshCompression":"Json","extension":null,"config":"","revision":1}
+                resolve(info)
+            })
+        }
+    
+
+        window.getView=function(){
+            return new Promise((resolve,reject)=>{
+                let info=[{"id":"69323","name":"国徽烘焙2021","override":"{\"pers\":false,\"fd\":0.0,\"nd\":0.0,\"td\":0.0,\"roff\":0.0,\"uoff\":0.0,\"hext\":0.0,\"vext\":0.0,\"org\":null,\"vdir\":null,\"udir\":null,\"rdir\":null,\"scale\":0,\"crpatv\":false,\"crpvbl\":false,\"crpmin\":null,\"crpmax\":null,\"secactive\":false,\"secmin\":null,\"secmax\":null,\"secorg\":null,\"viewrange\":null,\"secxdir\":null,\"secydir\":null,\"seczdir\":null,\"BoundingBox3dLeftDown\":null,\"BoundingBox3dLeftUp\":null,\"BoundingBox3dRightUp\":null,\"BoundingBox3dRightDown\":null,\"clipBoxes\":null,\"clipPolygons\":null,\"displaystyle\":null}","isDefault":true,"snapshot":null,"viewType":"3D","category":"3D"}]
+                resolve(info)
+            })
+        }
+    
+
+        window.getMaterial=function(){
+            return new Promise((resolve,reject)=>{
+                let info=[{"id":"AB585D7FB69F8225","content":"{\"cat\":\"Unassigned\",\"userenderappearance\":false,\"color\":\"CCCCCC\",\"appearancecolor\":\"CCCCCC\",\"tinttoggle\":false,\"tintcolor\":\"505050\",\"tp\":1.0,\"shi\":64,\"ismetal\":false,\"SelfIllumFiltermap\":\"FFFFFF\",\"SelfIllumLuminance\":0.0,\"SelfIllumColorTemperature\":6500.0,\"ReflectivityDirect\":0.0,\"ReflectivityOblique\":0.0,\"Glossness\":0.5,\"img\":null,\"uf\":0.0,\"vf\":0.0,\"usc\":1.0,\"vsc\":1.0,\"uvsc\":1.0,\"ang\":0.0,\"URepeat\":true,\"VRepeat\":true,\"typ\":null,\"othertextures\":null}","type":"3D"},{"id":"A8C88E61DCBAF23D","content":"{\"cat\":\"Unassigned\",\"userenderappearance\":false,\"color\":\"7F7F7F\",\"appearancecolor\":\"7F7F7F\",\"tinttoggle\":false,\"tintcolor\":\"505050\",\"tp\":1.0,\"shi\":1,\"ismetal\":false,\"SelfIllumFiltermap\":\"FFFFFF\",\"SelfIllumLuminance\":0.0,\"SelfIllumColorTemperature\":6500.0,\"ReflectivityDirect\":0.0,\"ReflectivityOblique\":0.0,\"Glossness\":0.5,\"img\":\"21d7cf8b628c9a8cc13eb15f62974ce2\",\"uf\":0.0,\"vf\":0.0,\"usc\":1.0,\"vsc\":1.0,\"uvsc\":1.0,\"ang\":0.0,\"URepeat\":true,\"VRepeat\":true,\"typ\":null,\"othertextures\":null}","type":"3D"},{"id":"A8C47C4CAA1B5EA3","content":"{\"cat\":\"Unassigned\",\"userenderappearance\":false,\"color\":\"7F7F7F\",\"appearancecolor\":\"7F7F7F\",\"tinttoggle\":false,\"tintcolor\":\"505050\",\"tp\":1.0,\"shi\":1,\"ismetal\":false,\"SelfIllumFiltermap\":\"FFFFFF\",\"SelfIllumLuminance\":0.0,\"SelfIllumColorTemperature\":6500.0,\"ReflectivityDirect\":0.0,\"ReflectivityOblique\":0.0,\"Glossness\":0.5,\"img\":\"7a8c8c8f61b1656767cc18c009ab4700\",\"uf\":0.0,\"vf\":0.0,\"usc\":1.0,\"vsc\":1.0,\"uvsc\":1.0,\"ang\":0.0,\"URepeat\":true,\"VRepeat\":true,\"typ\":null,\"othertextures\":null}","type":"3D"},{"id":"EFC19F8A036E3142","content":"{\"cat\":\"Unassigned\",\"userenderappearance\":false,\"color\":\"7F7F7F\",\"appearancecolor\":\"7F7F7F\",\"tinttoggle\":false,\"tintcolor\":\"505050\",\"tp\":1.0,\"shi\":1,\"ismetal\":false,\"SelfIllumFiltermap\":\"FFFFFF\",\"SelfIllumLuminance\":0.0,\"SelfIllumColorTemperature\":6500.0,\"ReflectivityDirect\":0.0,\"ReflectivityOblique\":0.0,\"Glossness\":0.5,\"img\":\"70412a07954a3f898addd3544e48d9dc\",\"uf\":0.0,\"vf\":0.0,\"usc\":1.0,\"vsc\":1.0,\"uvsc\":1.0,\"ang\":0.0,\"URepeat\":true,\"VRepeat\":true,\"typ\":null,\"othertextures\":null}","type":"3D"}]
+                resolve(info)
+            })
+        }
+    
+
+        window.getLink=function(){
+            return new Promise((resolve,reject)=>{
+                let info=[{"linkID":"0","linkName":"国徽烘焙2021.max","mainFile":false}]
+                resolve(info)
+            })
+        }
+    
+
+        window.getElements=function(){
+            return new Promise((resolve,reject)=>{
+                let info=[{"ID":"2","N":"SM_GH_1","TID":"","TN":"","FID":"","FN":"","CID":"","CN":"","LID":"","LKID":"0","LLID":null,"O":"","S":"","T":"Element"},{"ID":"3","N":"SM_GH_2","TID":"","TN":"","FID":"","FN":"","CID":"","CN":"","LID":"","LKID":"0","LLID":null,"O":"","S":"","T":"Element"},{"ID":"4","N":"SM_GH_3","TID":"","TN":"","FID":"","FN":"","CID":"","CN":"","LID":"","LKID":"0","LLID":null,"O":"","S":"","T":"Element"},{"ID":"5","N":"SM_GH_4","TID":"","TN":"","FID":"","FN":"","CID":"","CN":"","LID":"","LKID":"0","LLID":null,"O":"","S":"","T":"Element"},{"ID":"6","N":"SM_GH_5","TID":"","TN":"","FID":"","FN":"","CID":"","CN":"","LID":"","LKID":"0","LLID":null,"O":"","S":"","T":"Element"},{"ID":"7","N":"SM_GH_6","TID":"","TN":"","FID":"","FN":"","CID":"","CN":"","LID":"","LKID":"0","LLID":null,"O":"","S":"","T":"Element"},{"ID":"8","N":"SM_GH_7","TID":"","TN":"","FID":"","FN":"","CID":"","CN":"","LID":"","LKID":"0","LLID":null,"O":"","S":"","T":"Element"},{"ID":"9","N":"SM_GH_8","TID":"","TN":"","FID":"","FN":"","CID":"","CN":"","LID":"","LKID":"0","LLID":null,"O":"","S":"","T":"Element"}]
+                resolve(info)
+            })
+        }
+    
+
+        window.getBVHNodes=function(){
+            return new Promise((resolve,reject)=>{
+                let info=[{"nodeID":0,"parent":-1,"left":1,"right":2,"innerNode":true,"isTransparent":false,"mergeBlock":[{"id":"0_A8C88E61DCBAF23D_Mesh","prims":[{"id":"a38f0e7c1f358418","eid":"9","AABB":[0,-0.5658,-0.04,0.5025,0.5,0.0004]},{"id":"55d1625d1e0ea14c","eid":"2","AABB":[-0.50246894,-0.5657571,-0.040000003,0,0.5,0.00042060003]}],"type":0,"bs":[0.00001552701,-0.019789701,0.032900006,0.732722]}],"AABB":[-0.5024689435958862,-0.5899999737739563,-0.04000000283122063,0.5024999976158142,0.5,0.022000402212142944],"size":1.4838821773015483,"depth":0,"file":0},{"nodeID":1,"parent":0,"left":3,"right":-1,"innerNode":true,"isTransparent":false,"mergeBlock":[{"id":"1_EFC19F8A036E3142_Mesh","prims":[{"id":"9d114e9edc8faa03","eid":"7","AABB":[0,-0.59,-0.04,0.385,0.2356,0.022]}],"type":0,"bs":[0.1925,-0.009,0.17719999,0.45653158]},{"id":"1_A8C88E61DCBAF23D_Mesh","prims":[{"id":"49c815c8a1469203","eid":"6","AABB":[-0.026641602,-0.45090023,0.013,0.028769901,-0.39605984,0.022000402]}],"type":0,"bs":[0.0010641497,0.017500201,0.42348003,0.039239362]}],"AABB":[-0.3849993050098419,-0.5899999737739563,-0.04000000283122063,0.38499999046325684,0.23559999465942383,0.022000402212142944],"size":1.1306450491804554,"depth":1,"file":1},{"nodeID":2,"parent":0,"left":5,"right":-1,"innerNode":true,"isTransparent":false,"mergeBlock":[{"id":"2_A8C47C4CAA1B5EA3_Mesh","prims":[{"id":"a8d6778c81a678f7","eid":"5","AABB":[0,-0.5658,-0.01,0.5025,0.5,0.022]}],"type":0,"bs":[0.25125,0.006,0.032900006,0.58937675]},{"id":"2_A8C88E61DCBAF23D_Mesh","prims":[{"id":"af4f62557f706b0e","eid":"8","AABB":[-0.11412681,0.157918,0,0.11412681,0.37500003,0.016]}],"type":0,"bs":[0,0.008,-0.26645902,0.1577025]}],"AABB":[-0.5024673342704773,-0.5658000111579895,-0.010000000707805157,0.5024999976158142,0.5,0.02200000174343586],"size":1.4652347508119776,"depth":1,"file":2},{"nodeID":3,"parent":1,"left":-1,"right":-1,"innerNode":false,"isTransparent":false,"mergeBlock":[{"id":"3_EFC19F8A036E3142_Mesh","prims":[{"id":"a6f728d865f0c77","eid":"4","AABB":[-0.3849993,-0.5899767,-0.040000003,0,0.2355996,0.021955501]}],"type":0,"bs":[-0.19249965,-0.009022251,0.17718856,0.45651922]}],"AABB":[-0.3849993050098419,-0.5899767279624939,-0.04000000283122063,0,0.2355996072292328,0.021955501288175583],"size":0.9130384358662941,"depth":2,"file":3},{"nodeID":5,"parent":2,"left":-1,"right":-1,"innerNode":false,"isTransparent":false,"mergeBlock":[{"id":"5_A8C47C4CAA1B5EA3_Mesh","prims":[{"id":"4a7b2bbc7fe96fe7","eid":"3","AABB":[-0.50246733,-0.5657571,-0.010000001,0,0.5,0.022000002]}],"type":0,"bs":[-0.25123367,0.0060000005,0.032878548,0.58935034]}],"AABB":[-0.5024673342704773,-0.5657570958137512,-0.010000000707805157,0,0.5,0.02200000174343586],"size":1.1787007603015165,"depth":2,"file":4}]
+                resolve(info)
+            })
+        }
+    
